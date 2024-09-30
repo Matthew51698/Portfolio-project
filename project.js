b301ee2b4d94cbf638e8ab6ec59c8326e711a698
Matthew51698 <matthew_turner51698@outlook.com>
2024-09-30 22:45:24 +0200

    Create motivational quotes

---
+++ project.js
@@ -0,0 +1,10 @@
+let quotes = ['Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.',
+'Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.',
+'The future belongs to those who believe in the beauty of their dreams." — Eleanor Roosevelt',
+'Don’t watch the clock; do what it does. Keep going. — Sam Levenson',
+'Your limitation—its only your imagination.',
+'Dream it. Wish it. Do it.', 'Success doesn’t just find you. You have to go out and get it.',
+'The only way to achieve the impossible is to believe it is possible.',
+'Push yourself, because no one else is going to do it for you.',
+'Great things never come from comfort zones.'
+]
